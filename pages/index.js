a33113755607632fb283af1502efcbd381b849ba
DET171 <mrgeek484@gmail.com>
2022-03-15 19:08:29 +0800

    patch

--- pages/index.js
+++ pages/index.js
@@ -29,26 +29,6 @@ class App extends Component {
 	}
 
 	async handleCredentialResponse(response) {
-		/**
-	{
-   "web":{
-      "client_id":"790718621561-40q6f32jb9l4ug1iv4bjdgmbgbrtsub5.apps.googleusercontent.com",
-      "project_id":"fit-pathway-344209",
-      "auth_uri":"https://accounts.google.com/o/oauth2/auth",
-      "token_uri":"https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-      "client_secret":"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",
-      "javascript_origins":[
-      	"https://hci-science-project.github.io",
-        "http://localhost",
-        "http://localhost:3000"
-      ]
-   	}
-}
-		 */
-		const rawRes = await axios.get('https://www.googleapis.com/oauth2/v1/certs');
-		const certs = rawRes.data;
-		const certKeys = Object.keys(certs);
 		const cert = '-----BEGIN CERTIFICATE-----\nMIIDJjCCAg6gAwIBAgIIJMUg7I+7qIswDQYJKoZIhvcNAQEFBQAwNjE0MDIGA1UE\nAwwrZmVkZXJhdGVkLXNpZ25vbi5zeXN0ZW0uZ3NlcnZpY2VhY2NvdW50LmNvbTAe\nFw0yMjAzMDQxNTIxNDBaFw0yMjAzMjEwMzM2NDBaMDYxNDAyBgNVBAMMK2ZlZGVy\nYXRlZC1zaWdub24uc3lzdGVtLmdzZXJ2aWNlYWNjb3VudC5jb20wggEiMA0GCSqG\nSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCtfO33GkoLW+pu1Wb5cmLbKzX/gtqJEoGE\nE3JRO1MFl52Am2vddTpJKiXwcQPDMC49vdw4MAGvTCk2YQYWDt1xIvpA5JJveye2\n+yJirFRcgWcWkAYwQDZLNyicUt0Bos0oBfVPgXURTnS/bQQhkXFZ1HyxnQKWs6uL\nE9/9NHaFGiqLK+ukTJ5XHFZq1p7YM9OaB5OZ1qc8AqOylH2IXjESlGijYwpQYYwM\nNm8UlJKUvUF3bpJtcQdi+fia44ta5qujhVrYdQ+d8NckQ88CvYxqDWxuIQyCOzQV\nSV8mST+hmvwYQ6fLhM6WEaYmas8KS6caPfICbA6YNQumNGKGSeSnAgMBAAGjODA2\nMAwGA1UdEwEB/wQCMAAwDgYDVR0PAQH/BAQDAgeAMBYGA1UdJQEB/wQMMAoGCCsG\nAQUFBwMCMA0GCSqGSIb3DQEBBQUAA4IBAQBrTj3aIRxbH2QRlEgc/ZqMTQuOXqud\nd2CgMNoyTGO+uvMStHnLDIA6G31rN9mBFPrrCX4fbXsEcnw8diiyRMtk/H+MgbP5\nv4B0foDWkzzK9yG9JPS8uD60h7YSv/13flYHBvhG+lr5/nJEf1RNUo8gzGVNyEag\nLf1v/gMLLtqqvnTA7j9bu+ltj3WaD7nEd+MYgbUWDmfsbZJ8pEpSq+11c8OveEcB\naVlMcPw7zIJieAKTxmabcZsQYjEjU1CcWtuzzs2mnHy44Ow24REeZg5wzU0ZRI01\nnjfiSqbA4lUPd6X5cHPOpt7cFcnVElHWoiroQi38+BWWBunw/hl5/2Qy\n-----END CERTIFICATE-----\n';
 
 		console.log('Encoded JWT ID token: ' + response.credential);
